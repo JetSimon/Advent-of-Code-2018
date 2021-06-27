3d2ef65fa3edb2c08cdae070d290bf0aa6064ae9
Jet Simon <jetjdsimon@gmail.com>
2021-06-27 12:22:34 -0700

    1

--- day 1/day1.py
+++ day 1/day1.py
@@ -2,14 +2,28 @@ import itertools, re
 
 def loadData():
     f = open('input.txt')
-    out = [line.strip() for line in f]
+    out = [int(line.strip()) for line in f]
     f.close()
     return out
 
 data = loadData()
 
-def solve_1():
-    pass
+def solve_1(data):
+    return sum(data)
 
-def solve_2():
-    pass
+def solve_2(data):
+    seen = {0:True}
+    f = 0
+    i = 0
+    while True:
+        n = data[i]
+        f += n
+        if f in seen:
+            return f
+        seen[f] = True
+        i += 1
+        if i >= len(data):
+            i = 0
+
+print(solve_1(data))
+print(solve_2(data))

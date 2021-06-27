2c4675488a00c891a03e2c47903e1c319ddaaaf7
Jet Simon <jetjdsimon@gmail.com>
2021-06-27 12:36:30 -0700

    2

--- day 2/day2.py
+++ day 2/day2.py
@@ -8,8 +8,37 @@ def loadData():
 
 data = loadData()
 
-def solve_1():
-    pass
+def times(s, counts):
+    s = list(s)
+    for c in set(s):
+        count = s.count(c)
+        if count in counts:
+            counts[count] = True
 
-def solve_2():
-    pass
+def difference(a,b):
+    same = ""
+    for i in range(len(a)):
+        if a[i] == b[i]:
+            same += a[i]
+    return len(a) - len(same), same
+
+def solve_1(data):
+    three = 0
+    two = 0
+    for code in data:
+        counts = {2:False, 3:False}
+        times(code, counts)
+        two += 1 if counts[2] else 0
+        three += 1 if counts[3] else 0
+    return two * three
+
+def solve_2(data):
+    for code in data:
+        for other in data:
+            n, same = difference(code, other)
+            if n == 1:
+                return same
+    return None
+
+print(solve_1(data))
+print(solve_2(data))

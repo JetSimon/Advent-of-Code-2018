a56a19878b965de6b1f2664d0baa7b1a145ff16b
Jet Simon <jetjdsimon@gmail.com>
2021-06-28 21:26:30 -0700

    5
    slow

--- day 5/day5.py
+++ day 5/day5.py
@@ -8,8 +8,57 @@ def loadData():
 
 data = loadData()
 
-def solve_1():
-    pass
+def removeAll(s,i):
+    out = ""
+    for c in s:
+        if c.lower() == i.lower():
+            continue
+        out += c
+    return out
 
-def solve_2():
-    pass
+def destroyPolarity(s):
+    out = ""
+    skip = False
+    for i in range(len(s)):
+        if i == len(s) - 1:
+            if not skip:
+                out += s[i]
+            break
+        c = s[i]
+        n = s[i+1]
+        if not skip:
+            if c.lower() == n.lower() and ( (c.isupper() and n.islower()) or (c.islower() and n.isupper()) ):
+                skip = True
+            else:
+                out += c
+        else:
+            skip = False
+    return out
+
+def reactionLen(s):
+    n = destroyPolarity(s)
+    while n != s:
+        s = n
+        n = destroyPolarity(s)
+    return len(n)
+
+def solve_1(s):
+    n = destroyPolarity(s)
+    while n != s:
+        s = n
+        n = destroyPolarity(s)
+    return n
+
+def solve_2(s):
+    shortest = len(s)
+    removed = None
+    for c in set(s.lower()):
+        t = removeAll(s,c)
+        att = reactionLen(t)
+        if att < shortest:
+            shortest = att
+            removed = c
+        print("done")
+    return shortest
+
+print(solve_2(data[0]))

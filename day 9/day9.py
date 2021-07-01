53793480213f0756000ff933806f99febb623087
Jet Simon <jetjdsimon@gmail.com>
2021-06-30 19:15:49 -0700

    9
    actually let brute force pt 2 run for an hour lol

--- day 9/day9.py
+++ day 9/day9.py
@@ -1,4 +1,5 @@
 import itertools, re
+from math import floor
 
 def loadData():
     f = open('input.txt')
@@ -6,10 +7,34 @@ def loadData():
     f.close()
     return out
 
-data = loadData()
 
-def solve_1():
-    pass
+data = loadData()
+lastPer = 0
+def solve_1(playerAmount, final):
+    global lastPer
+    pos = 0
+    board = [0]
+    players = [0] * playerAmount
+    playerIndex = 0
+    for i in range(1,final+1):
+        per = floor((i / final) * 100)
+        if per != lastPer:
+            lastPer = per
+            print(per,"% done")
+        if i % 23 != 0:
+            pos = ((pos + 1) % len(board)) + 1
+            board.insert(pos, i)
+        else:
+            players[playerIndex % len(players)] += i + board[pos-7]
+            del board[pos - 7]
+            pos = pos - 7
+            if pos < 0:
+                pos += len(board)+1
+        playerIndex += 1
+        #print("[" + str(i) + "]", board, "current: ", board[pos])
+    print(max(players))
 
 def solve_2():
     pass
+
+print( solve_1(424, 71482* 100) )
